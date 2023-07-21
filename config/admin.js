8c3e06a17b44c03ba8ddbe90e169497c8f2efbfb
Kyman <info@kymancheng.nl>
2023-07-21 15:49:06 +0200

    update info

--- config/admin.js
+++ config/admin.js
@@ -2,6 +2,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'e4350524b0689fc3f46e2407dae950e2'),
   },
+  url: env('PUBLIC_ADMIN_URL', process.env.PUBLIC_ADMIN_URL),
   apiToken: {
     salt: env('API_TOKEN_SALT'),
   },
--- config/server.js
+++ config/server.js
@@ -1,7 +1,9 @@
 module.exports = ({ env }) => ({
   host: env('HOST', '0.0.0.0'),
+  url: env('PUBLIC_URL', process.env.PUBLIC_URL),
+  proxy: true,
   port: env.int('PORT', 1337),
   app: {
     keys: env.array("APP_KEYS"),
-  },
+  }
 });
